f80ee06c2a1488d67c17c384c78df8e954e637e8
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-04-11 23:41:53 +0530

    chat

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:8000";
+export const host = "https://chat-app-v3rm.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
