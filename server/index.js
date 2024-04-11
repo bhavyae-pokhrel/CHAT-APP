4c02920a1620e145863a71341abc34b0d1a80332
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-04-11 18:32:54 +0530

    chat

--- server/index.js
+++ server/index.js
@@ -6,12 +6,15 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
-
 app.use(cors());
 app.use(express.json());
-
+const MONGO_URL="mongodb+srv://bhavyaeprasadpokhrel:12312312@cluster0.xxzpmqc.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
+const port=5000
+const ORIGIN="http://localhost:3001"
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(
+    MONGO_URL
+    , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -25,12 +28,12 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+const server = app.listen(port, () =>
+  console.log(`Server started on ${port}`)
 );
 const io = socket(server, {
   cors: {
-    origin: process.env.ORIGIN,
+    origin:ORIGIN ,
     credentials: true,
   },
 });
